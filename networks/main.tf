3cfa096b8a91d13b51a4dea1dc7633c8f3dbbdb4
Wellington <rs.wellington@yahoo.com.br>
2023-02-06 12:04:37 -0300

    Implementação router, nat, route.

--- networks/main.tf
+++ networks/main.tf
@@ -21,3 +21,28 @@ resource "google_compute_subnetwork" "subnet" {
   private_ip_google_access = true
   region                   = var.region
 }
+
+resource "google_compute_router" "router" {
+  name    = "${local.network_name}-router"
+  network = google_compute_network.vpc.name
+  region  = google_compute_subnetwork.subnet.region
+}
+
+resource "google_compute_router_nat" "nat_router" {
+  name                               = "${google_compute_subnetwork.subnet.name}-nat-router"
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  router                             = google_compute_router.router.name
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  subnetwork {
+    name                    = google_compute_subnetwork.subnet.name
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+}
+
+resource "google_compute_route" "egress_internet" {
+  dest_range = "0.0.0.0/0"
+  name       = "egress-internet"
+  network    = google_compute_network.vpc.name
+  next_hop_gateway = "default-internet-gateway"
+}
